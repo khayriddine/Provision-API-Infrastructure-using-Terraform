caa4b8908048211accd973203c48bcef15ca18d5
Khayriddine Battikh <khayriddine.battikh@emerson.com>
2021-08-21 09:30:05 +0100

    add main terraform & update gitignore

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "azurerm"{
+    version = "~>2.5.0"
+    features {
+
+    }
+}
+
+resource "azurerm_resource_group" "tfgroup"{
+    name ="tfmainrg"
+    location="westeurope"
+}
+
+resource "azurerm_container_group" "tfcg"{
+    name ="weatherapi"
+    location = azurerm_resource_group.tfgroup.location
+    resource_group_name=azurerm_resource_group.tfgroup.name
+
+    ip_address_type= "public"
+    dns_name_label= "myweatherapiterraform"
+    os_type ="Linux"
+
+    container{
+        name ="weatherapi"
+        image ="khayriddine/weatherapi"
+        cpu ="1"
+        memory= "1"
+
+        ports{
+            port = 80
+            protocol ="TCP"
+        }
+    }
+}
